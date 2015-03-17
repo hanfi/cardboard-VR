eec65cf2aa8e81d0ea1e11df19a0b79a77b7d6df
Souhail Hanfi <souhail.hanfi@neoxia.com>
2015-03-17 17:14:49 +0100

    some test

--- js/Room4.js
+++ js/Room4.js
@@ -12,7 +12,7 @@ function Room4(scene){
 	this.hotspotToRoom3 = new Hotspot(scene, {
 		rectLength: 4, 
 		rectWidth: 4, 
-		degree: 270,
+		degree: 130,
 		verticalDegree: 0,
 		radius: 5 * SCALE,
 		showHotspot: scene.showHotspots,
@@ -25,53 +25,9 @@ function Room4(scene){
 		}
 	});
 	
-	this.hotspotToRoom5 = new Hotspot(scene, {
-		rectLength: 4, 
-		rectWidth: 4, 
-		degree: 90,
-		verticalDegree: 0,
-		radius: 5 * SCALE,
-		showHotspot: scene.showHotspots,
-		onFocus: function(){
-				// remove the room
-				room4.remove();
-				// Start a new room
-				var room = new GameRoom(scene);
-				scene.add(room);
-		}
-	});
 	
-	this.textbox = new Textbox(scene, {
-		text: "We love technology",
-		radius: 5 * SCALE,
-		degree: 50,
-		verticalDegree: 165,
-		color: 0xeeeeee
-	});
 	
-	this.textBoxes = [];
-	
-	this.smallText = function(text, verticalDegree){
-		var textbox = new Textbox(scene, {
-			text: text,
-			radius: 5 * SCALE,
-			degree: 50,
-			verticalDegree: verticalDegree,
-			color: 0xeeeeee,
-			size: 1
-		});
-		this.textBoxes.push(textbox);
-		return textbox;
-	};
-	
-	this.smallText("We know that technology can make a difference in ", 176);
-	this.smallText("business, society, and life in general. We seek relevance", 182);
-	this.smallText("through embracing new information technology and using", 188);
-	this.smallText("this to innovate for and with our customers. We are pioneers", 194);
-	this.smallText("in open innovation. We proactively develop modular,", 200);
-	this.smallText("reusable software components.", 206);
-	
-	this.photoSphere = new PhotoSphere(scene, 'images/small.jpg', {  depth: 110 });
+	this.photoSphere = new PhotoSphere(scene, 'images/bureau.jpg', {  depth: 110 });
 	
 	var sphere = new THREE.Mesh(
 		new THREE.SphereGeometry(100, 32, 32),
@@ -92,13 +48,8 @@ function Room4(scene){
 	this.remove = function(){
 		this.photoSphere.remove();
 		this.textLights.remove();
-		this.textbox.remove();
 		this.hotspotToRoom3.remove();
-		this.hotspotToRoom5.remove();
 		
-		for (var i=0;i<this.textBoxes.length;i++){
-			this.textBoxes[i].remove();
-		}
 		
 		// remove self
 		scene.removeRoom(this);
